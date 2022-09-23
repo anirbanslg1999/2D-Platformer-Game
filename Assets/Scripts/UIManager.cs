e7312ce20a3193684759ce0ce10509d768e5f4f4
anirbanslg1999 <anirbanslg1999@gmail.com>
2022-09-23 18:23:48 +0530

    Added UI

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,24 +6,39 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("Sprites")]
     [SerializeField] List<Image> healthImg;
     [SerializeField] Sprite healthLostIcon;
+    [Header("Panels")]
+    [SerializeField] GameObject gameEndPanel;
+    [SerializeField] GameObject inGamePanel;
     private int healthCountIndex = 0;
+
+    private void Start()
+    {
+        inGamePanel.SetActive(true);
+        gameEndPanel.SetActive(false);
+    }
 
     public void DecrementHealthCout()
     {
-        if(healthCountIndex < healthImg.Count - 1)
+        if (healthCountIndex < healthImg.Count - 1)
         {
             healthImg[healthCountIndex].sprite = healthLostIcon;
             healthCountIndex++;
         }
         else
         {
-            PlayerDead();
+            inGamePanel.SetActive(false);
+            gameEndPanel.SetActive(true);
         }
     }
-    void PlayerDead()
+    public void ReloadScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
